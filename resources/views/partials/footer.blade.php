cf811c7a74296396d3ea9c209a6d3b4142acf4f7
Flavia <flaviamatta90@gmail.com>
2020-10-20 16:37:57 +0200

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="footer-container">
-        <div class="footer-left">
+        <div class="footer-list">
             <img class="logo" src="https://cdn.shortpixel.ai/client/q_lossy,ret_img/https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="logo">
             <ul>
                 <li>Regione sociale: La Molisana S.P.A</li>
@@ -17,11 +17,42 @@
             </ul>
     
         </div>
-        <div class="footer-center">
-    
+        <div class="footer-list">
+            <h2>PASTIFICIO</h2>
+            <ul>
+                <li>Il Pastificio</li>
+                <li>Grano decorato a pietra</li>
+                <li>Il Molise c'è</li>
+                <li>Filiera integrata</li>
+                <li>100 anni di pasta</li>
+                <li>Sartoria della pasta</li>
+                <li>Spaghetti quadrato</li>
+                <li>Le Persone</li>
+            </ul>
+            <h2>PRODOTTI</h2>
+            <ul>
+                <li>Le Classiche</li>
+                <li>Le Integrali</li>
+                <li>Le Speciali</li>
+                <li>Le Biologiche</li>
+                <li>Le Gluten-Free</li>
+                <li>Sartoria della pasta</li>
+                <li>Le Semole</li>
+                <li>Le Extra di Lusso</li>
+            </ul>
         </div>
-        <div class="footer-right">
-    
+        <div class="footer-list">
+            <h2>COLLEZIONE DA CHEF</h2>
+            <ul>
+                <li>Collezione da Chef</li>
+                <li>Grandi Cucine</li>
+                <li>Biologiche</li>
+                <li>Quadrate</li>
+                <li>100 anni di pasta</li>
+                <li>Sartoria della pasta</li>
+                <li>Spaghetti quadrato</li>
+                <li>Le Persone</li>
+            </ul>
         </div>
     </div>
     
