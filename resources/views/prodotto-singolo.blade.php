ddd1fe2086a0beac82ccce6226cdc11b878daa09
Flavia <flaviamatta90@gmail.com>
2020-10-22 17:33:29 +0200

    with arrow

--- resources/views/prodotto-singolo.blade.php
+++ resources/views/prodotto-singolo.blade.php
@@ -5,17 +5,23 @@
     <div class="monocromo">
         <h2>{{$data["titolo"]}}</h2>
         <img class= "singoli-prodotti" src="{{$data["src-h"]}}" alt="pasta">
+        <div class="arrow">
+            @if ($id > 0)
+                <a href="{{route('dettaglio-prodotto', $id - 1)}}">
+                <i class="fas fa-angle-left"></i></a>
+         @endif
+
+            @if ($id < $length - 1)
+                <a href="{{route('dettaglio-prodotto', $id + 1)}}">
+                <i class="fas fa-angle-right"></i> </a>
+            @endif
+        </div>
+        
         <img class = "singoli-prodotti" src="{{$data["src-p"]}}" alt="pasta">
         <p>{!!$data["descrizione"]!!}</p>    
     </div>
 
-    @if ($id > 0)
-        <a href="{{route('dettaglio-prodotto', $id - 1)}}">Prev</a>
-    @endif
-    @if ($id < $length - 1)
-        
-    @endif
-    <a href="{{route('dettaglio-prodotto', $id + 1)}}">Next</a>
+    
 
     
 @endsection
